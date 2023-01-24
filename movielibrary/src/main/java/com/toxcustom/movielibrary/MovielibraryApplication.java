58c0857fd27dd3d7a7faf728eeb9d14d042b1d9a
toXCustom <pawel89@gmail.com>
2023-01-24 12:46:23 +0100

    Initial commit

---
+++ movielibrary/src/main/java/com/toxcustom/movielibrary/MovielibraryApplication.java
@@ -0,0 +1,13 @@
+package com.toxcustom.movielibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovielibraryApplication.class, args);
+	}
+
+}
